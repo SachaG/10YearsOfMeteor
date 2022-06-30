e667f202008852a8cb046a8ee673179bc32573e7
Sacha Greif <SachaG@users.noreply.github.com>
2022-06-30 11:29:25 +0900

    title

--- netlify/functions/rebuild/rebuild.js
+++ netlify/functions/rebuild/rebuild.js
@@ -1,7 +1,7 @@
-// const { schedule } = require('@netlify/functions');
+const { schedule } = require('@netlify/functions');
 // const fetch = require('node-fetch');
 import fetch from 'node-fetch';
-import { schedule } from '@netlify/functions';
+// import { schedule } from '@netlify/functions';
 
 const REBUILD_URL = process.env.BUILD_HOOK_URL;
 
